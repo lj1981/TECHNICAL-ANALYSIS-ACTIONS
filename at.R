035851cf0d81cdb369b9514fecdcb53e6ca62bf6
Luiz Cunha - Maverick <97998057+lj1981@users.noreply.github.com>
2024-02-06 11:07:40 -0300

    Atualizaçâo com grafico

--- at.R
+++ at.R
@@ -1,21 +1,26 @@
 library(quantmod)
 
 getSymbols("PEP", src="yahoo", from='2024-01-01', to='2024-02-05')
-periodicity(PEP)
-CLASS(PEP)
-head(PEP, n=6 )
+periodicity(PEP) #Funçâo do pacote XPM 
+CLASS(PEP)  #Formato dos dados 
+head(PEP, n=6 ) # Visualizar os dados 
 
-Op(PEP)
-Hi(PEP)
-Lo(PEP)
-Cl(PEP)
-Vo(PEP)
-aD(PEP)
+Op(PEP) #Valor de abertura 
+Hi(PEP) #Valor mais alto 
+Lo(PEP) #Valor mais baixo 
+Cl(PEP) #Valor de fechamento 
+Vo(PEP) #Volume
+Ad(PEP) #Valor real já ajustado 
 
-HLC(PEP)
-OHLC(PEP)
+HLC(PEP) # Colunas Alta,Baixa,Fechamento  
+OHLC(PEP) # Colunas abertura,Valor mais alto,Fechamento
+OHLCV(PEP) #Colunas Abertura,VMA,Fechamento,Volume
 
-OpCl(PEP)
+OpCl(PEP) #Percentua do valor de abertura e de fechamento 
+HiCl(PEP) #Valor mais altoe de fechamento
+
+ClCl(PEP)#Compara periodo de fechamento com um anterior (dois dias)
+OpOp(PEP)#Compara periodo de abertura ''
 
 
-
+plot(Hi(PEP),col="Green", lwd=2)
