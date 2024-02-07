4273eab77c1baf518c7bee09385f2f1d4fc0d6d2
Luiz Cunha - Maverick <97998057+lj1981@users.noreply.github.com>
2024-02-07 13:17:55 -0300

    Grafico
    addADX(n=14,maType = 'EMA',wilder = T)
    addSAR(accel = c(0.02, 0.2), col = "red")

--- ANG.R
+++ ANG.R
@@ -7,4 +7,13 @@ getSymbols("PBR", src = "yahoo", from = "2023-03-01", to = "2024-02-05")
 # Graficar a série temporal
 chartSeries(PBR, type = "line", theme = "white", TA = NULL)
 
+chartSeries(PBR, type = "candlesticks", theme = "white", TA = NULL)
+
+ADX(HLC(PBR), n=14)#Media movel do indice 
+
+addADX(n=14,maType = 'EMA',wilder = T)
+addSAR(accel = c(0.02, 0.2), col = "red")
+
+
+
 
